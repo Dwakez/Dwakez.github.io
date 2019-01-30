662b17a46abee43e8ee6a52cbcecf1bbd561f6ae
Dwakez <dwakez250@gmail.com>
2019-01-30 09:38:24 +0700

    Diagonal Direction move added

--- playground/script.js
+++ playground/script.js
@@ -6,38 +6,41 @@ var x = canvas.width / 2;
 var y = canvas.height / 2;
 
 var ballRadius = 10;
-var upPressed = false
-var downPressed = false
-var leftPressed = false
-var rightPressed = false
+var upPressed = false;
+var downPressed = false;
+var leftPressed = false;
+var rightPressed = false;
+var map = {};
 
-function keyDownHandler(e) {
-    if (e.keyCode == 68) {
-        rightPressed = true;
-    }
-    else if (e.keyCode == 65) {
-        leftPressed = true;
-    }
-    else if (e.keyCode == 87) {
+onkeydown = function (e) {
+    map[e.keyCode] = e.type == 'keydown';
+    if (e.keyCode === 87) {
         upPressed = true
     }
-    else if (e.keyCode == 83) {
+    if (e.keyCode === 83) {
         downPressed = true
+    }
+    if (e.keyCode === 65) {
+        leftPressed = true;
+    }
+    if (e.keyCode === 68) {
+        rightPressed = true;
     }
 }
 
-function keyUpHandler(e) {
-    if (e.keyCode == 68) {
-        rightPressed = false;
-    }
-    else if (e.keyCode == 65) {
-        leftPressed = false;
-    }
-    else if (e.keyCode == 87) {
+onkeyup = function (e) {
+    map[e.keyCode] = e.type == 'keyup';
+    if (e.keyCode === 87) {
         upPressed = false
     }
-    else if (e.keyCode == 83) {
+    if (e.keyCode === 83) {
         downPressed = false
+    }
+    if (e.keyCode === 65) {
+        leftPressed = false;
+    }
+    if (e.keyCode === 68) {
+        rightPressed = false;
     }
 }
 
@@ -60,18 +63,16 @@ function move() {
     if (rightPressed && x < canvas.width - ballRadius) {
         x += 2
     }
-    else if (leftPressed && x - ballRadius > 0) {
+    if (leftPressed && x - ballRadius > 0) {
         x -= 2
     }
-    else if (upPressed && y - ballRadius > 0) {
+    if (upPressed && y - ballRadius > 0) {
         y -= 2
     }
-    else if (downPressed && y < canvas.height - ballRadius) {
+    if (downPressed && y < canvas.height - ballRadius) {
         y += 2
     }
 
 }
 
-document.addEventListener("keydown", keyDownHandler, false);    // Key Pressed
-document.addEventListener("keyup", keyUpHandler, false);        // Key Unpressed
 setInterval(move, 10);
